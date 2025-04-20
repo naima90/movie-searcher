b8d252c36d47981899cf39c6fea03e235a5a845e
naima <naima@Naimas-MacBook-Air.local>
2025-04-20 20:39:22 +0100

    feat: adds movie list

---
+++ packages/src/components/MovieCard.tsx
@@ -0,0 +1,29 @@
+interface Movie {
+  title: string;
+  releaseDate: string;
+  url: string;
+}
+
+export const MovieCard = ({ movie }: { movie: Movie }) => {
+  const handleOnClick = () => {
+    alert("clicked!!");
+  };
+  return (
+    <>
+      <div className="movie-card">
+        <div className="movie-poster">
+          <img src={movie.url} alt={movie.title} />
+          <div className="movie-overlay">
+            <button className="favourite-btn" onClick={handleOnClick}>
+              🤍
+            </button>
+          </div>
+        </div>
+        <div className="movie-info">
+          <h3>{movie.title}</h3>
+          <p>{movie.releaseDate}</p>
+        </div>
+      </div>
+    </>
+  );
+};
