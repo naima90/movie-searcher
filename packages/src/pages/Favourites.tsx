a0c109c7ab0d041d0be863fa4e97ef477528a5c8
Naima <naimaosman203@gmail.com>
2025-05-17 18:56:44 +0100

    feat: displays fav movies in favourites page

--- packages/src/pages/Favourites.tsx
+++ packages/src/pages/Favourites.tsx
@@ -1,7 +1,20 @@
+import { MovieCard } from "../components/MovieCard";
+import { useMovieContext } from "../contexts/MovieContext";
 import "../css/Favourites.css";
 
 export const Favourites = () => {
-  return (
+  const { favourites } = useMovieContext();
+
+  return favourites.length ? (
+    <div className="favourites">
+      <h2>Your Favourites</h2>
+      <div className="movies-grid">
+        {favourites.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  ) : (
     <div className="favourites-empty">
       <h2>No Favourites Yet</h2>
       <p>Add a list of favourite movies to be displayed here.</p>
