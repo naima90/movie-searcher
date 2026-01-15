f4cb5c72737723d405646c630d28e7949be41daa
Naima <naimaosman203@gmail.com>
2026-01-15 19:55:34 +0000

    add default vals

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,23 @@
-variable "aws_region" { type = string }
-variable "project_name" { type = string }
-variable "domain_name" { type = string }
-variable "subdomain" { type = string }
-variable "alb_ssl_policy" { type = string }
+variable "aws_region" { 
+  type = string
+  default = "eu-west-2"
+  }
+variable "project_name" { 
+  type = string
+  default = "movie-searcher-app"
+  }
+variable "domain_name" { 
+  type = string
+  default = "naimaosmann.com"
+  }
+variable "subdomain" { 
+  type = string
+  default = "tm"
+  }
+variable "alb_ssl_policy" { 
+  type = string
+  default = "ELBSecurityPolicy-2016-08" 
+  }
 variable "container_port" {
   type    = number
   default = 8080
